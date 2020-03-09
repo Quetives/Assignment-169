13a1f6ded801158f3cbc3573e88c0802dc0209c7
Richard <40587587+Quetives@users.noreply.github.com>
2020-03-09 09:59:51 -0700

    Add project files.

---
+++ Assignment 169/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_169
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employeeOne = new Employee() { firstName = "Sample", lastName = "Student"};
+            employeeOne.SayName();
+            Console.ReadLine();
+
+        }
+    }
+}
